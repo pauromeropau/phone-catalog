5d130f4ee2616d6d6d9f97777893bd433d96a173
Paula Romero <pauromeropau@gmail.com>
2021-04-13 23:46:25 +0200

    20210413 - dockerized

--- server/index.js
+++ server/index.js
@@ -12,9 +12,11 @@ app.listen(PORT, () => {
 app.use(cors());
 
 app.get("/phones", (req, res) => {
+
   res.status(200).send({
     success: "true",
     message: "Data collected",
     data: phones,
   });
+  
 });
